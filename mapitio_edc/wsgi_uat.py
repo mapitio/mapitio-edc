5caf742e769032d22386fb285738c0254fdbe6ac
erikvw <ew2789@gmail.com>
2020-04-08 08:41:10 -0500

    fix gunicorn reference to app

--- mapitio_edc/wsgi_uat.py
+++ mapitio_edc/wsgi_uat.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mapitio.settings.uat")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mapitio_edc.settings.uat")
 
 application = get_wsgi_application()
