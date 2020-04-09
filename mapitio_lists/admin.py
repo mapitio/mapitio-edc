6e19a117edd72bbfbbbe47e5bdf2bd43672c873a
erikvw <ew2789@gmail.com>
2020-04-09 05:42:39 -0500

    add admin classes for lists

--- mapitio_lists/admin.py
+++ mapitio_lists/admin.py
@@ -5,6 +5,10 @@ from .admin_site import mapitio_lists_admin
 from .models import (
     Conditions,
     OffstudyReasons,
+    HypertensionTreatment,
+    DiabetesTreatment,
+    VisitReasons,
+    ArvRegimens,
 )
 
 
@@ -15,4 +19,24 @@ class ConditionsAdmin(ListModelAdminMixin, admin.ModelAdmin):
 
 @admin.register(OffstudyReasons, site=mapitio_lists_admin)
 class OffstudyReasonsAdmin(ListModelAdminMixin, admin.ModelAdmin):
+    pass
+
+
+@admin.register(HypertensionTreatment, site=mapitio_lists_admin)
+class HypertensionTreatmentAdmin(ListModelAdminMixin, admin.ModelAdmin):
+    pass
+
+
+@admin.register(ArvRegimens, site=mapitio_lists_admin)
+class ArvRegimensAdmin(ListModelAdminMixin, admin.ModelAdmin):
+    pass
+
+
+@admin.register(VisitReasons, site=mapitio_lists_admin)
+class VisitReasonsAdmin(ListModelAdminMixin, admin.ModelAdmin):
+    pass
+
+
+@admin.register(DiabetesTreatment, site=mapitio_lists_admin)
+class DiabetesTreatmentAdmin(ListModelAdminMixin, admin.ModelAdmin):
     pass
