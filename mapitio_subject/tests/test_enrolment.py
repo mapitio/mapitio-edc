65d7234b0607b09e7e582a33683262c91e5b5f2e
erikvw <ew2789@gmail.com>
2020-04-29 23:03:57 -0500

    remove enrollment group

--- mapitio_subject/tests/test_enrolment.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase, tag
-from model_bakery.baker import make_recipe
-
-
-class TestEnrolment(TestCase):
-    @tag("1")
-    def test_(self):
-        enrollment = make_recipe("mapitio_subject.enrollment")
