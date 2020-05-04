aaead34ca87c650722f71f0d0f42517886461b27
erikvw <ew2789@gmail.com>
2020-05-04 10:26:37 -0500

    revert changes to listboard

--- mapitio_subject/migrations/0004_auto_20200504_1746.py
+++ mapitio_subject/migrations/0004_auto_20200504_1746.py
@@ -6,28 +6,46 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mapitio_subject', '0003_auto_20200504_1617'),
+        ("mapitio_subject", "0003_auto_20200504_1617"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='deathreport',
-            name='death_cause_known',
-            field=models.CharField(choices=[('Yes', 'Yes'), ('No', 'No')], default='No', max_length=15, verbose_name='Is the cause of death known?'),
+            model_name="deathreport",
+            name="death_cause_known",
+            field=models.CharField(
+                choices=[("Yes", "Yes"), ("No", "No")],
+                default="No",
+                max_length=15,
+                verbose_name="Is the cause of death known?",
+            ),
         ),
         migrations.AddField(
-            model_name='historicaldeathreport',
-            name='death_cause_known',
-            field=models.CharField(choices=[('Yes', 'Yes'), ('No', 'No')], default='No', max_length=15, verbose_name='Is the cause of death known?'),
+            model_name="historicaldeathreport",
+            name="death_cause_known",
+            field=models.CharField(
+                choices=[("Yes", "Yes"), ("No", "No")],
+                default="No",
+                max_length=15,
+                verbose_name="Is the cause of death known?",
+            ),
         ),
         migrations.AlterField(
-            model_name='deathreport',
-            name='death_cause',
-            field=models.TextField(blank=True, null=True, verbose_name='The cause of death as recorded in the patient notes'),
+            model_name="deathreport",
+            name="death_cause",
+            field=models.TextField(
+                blank=True,
+                null=True,
+                verbose_name="The cause of death as recorded in the patient notes",
+            ),
         ),
         migrations.AlterField(
-            model_name='historicaldeathreport',
-            name='death_cause',
-            field=models.TextField(blank=True, null=True, verbose_name='The cause of death as recorded in the patient notes'),
+            model_name="historicaldeathreport",
+            name="death_cause",
+            field=models.TextField(
+                blank=True,
+                null=True,
+                verbose_name="The cause of death as recorded in the patient notes",
+            ),
         ),
     ]
